c5b49d95c646c018275429caba17213856dc496d
ilyabru <ilyabru@gmail.com>
2024-04-05 14:59:28 -0400

    Added Keychron q6 led changes

--- keyboards/keychron/q6/q6.c
+++ keyboards/keychron/q6/q6.c
@@ -66,18 +66,34 @@ bool rgb_matrix_indicators_advanced_kb(uint8_t led_min, uint8_t led_max) {
 #    if defined(CAPS_LOCK_LED_INDEX)
     if (host_keyboard_led_state().caps_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(41, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(42, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(62, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(78, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(79, 255, 255, 255);
     } else {
         if (!rgb_matrix_get_flags()) {
             RGB_MATRIX_INDICATOR_SET_COLOR(CAPS_LOCK_LED_INDEX, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(41, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(42, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(62, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(78, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(79, 0, 0, 0);
         }
     }
 #    endif // CAPS_LOCK_LED_INDEX
 #    if defined(NUM_LOCK_LED_INDEX)
     if (host_keyboard_led_state().num_lock) {
         RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(38, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(58, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(59, 255, 255, 255);
     } else {
         if (!rgb_matrix_get_flags()) {
             RGB_MATRIX_INDICATOR_SET_COLOR(NUM_LOCK_LED_INDEX, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(38, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(58, 0, 0, 0);
+            RGB_MATRIX_INDICATOR_SET_COLOR(59, 0, 0, 0);
         }
     }
 #    endif // NUM_LOCK_LED_INDEX
